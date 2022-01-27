5e5730fb8d0c5ca988c199bff136322764f0ff11
emuroku <m610310@gmail.com>
2022-01-28 02:41:17 +0900

    #1 結果の取得

--- html/top.php
+++ html/top.php
@@ -5,7 +5,10 @@ header('X-Frame-Options:DENY'); // クリックジャッキング対策
 require_once '../conf/const.php';
 
 // 汎用関数ファイル読み込み
-// require_once MODEL_PATH . 'functions.php';
+require_once MODEL_PATH . 'functions.php';
+
+// CSRF対策
+$token = get_csrf_token();
 
 // topページのクライアントソースファイル読み込み
 include_once VIEW_PATH . 'top_view.php';
