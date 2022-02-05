29215f431ff6b7972b094afab8e9a88129ccb713
emuroku <m610310@gmail.com>
2022-02-05 21:29:21 +0900

    #2 Twitterシェア機能を追加

--- view/result_view.php
+++ view/result_view.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>あなたと相性最強なNPB選手占い</title>
+    <title>あなたと相性が最強な野球選手診断</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php print(STYLESHEET_PATH . '/result.css'); ?>">
@@ -16,7 +16,7 @@
     <!-- navigation -->
     <nav class="navbar navbar-expand-sm justify-content-center navbar-light bg-light">
         <i class="fas fa-heart fa-2x"></i>
-        <h2> あなたと相性最強なNPB選手占い</h2><i class="fas fa-heart fa-2x"></i>
+        <h2> あなたと相性が最強な野球選手診断 </h2><i class="fas fa-heart fa-2x"></i>
         <!-- <i class="fas fa-home"></i> -->
     </nav>
 
@@ -38,35 +38,26 @@
             <section>
                 <div class="result_player">
                     <h5><i class="fas fa-baseball-ball"></i>あなたと相性が最強なのは...</h5>
-                        <div class="col-12">
+                    <div class="col-12">
 
-                            <div class="player_info">
-                                <div class="player_team">
-                                        <?php print($player_result['team']); ?>
-                                </div>
-                                <div class="player_name">
-                                        <?php print($player_result['name']); ?>
-                                </div>
-                                <div class = "player_type">タイプ：
-                                        <?php print($player_result['text']); ?>
+                        <div class="player_info">
+                            <div class="player_type">タイプ：
+                                <?php print($player_result['text']); ?>
+                            </div>
+                            <div class="player_team">
+                                <?php print($player_result['team']); ?>
+                            </div>
+                            <div class="player_name">
+                                <?php print($player_result['name']); ?>
                             </div>
                         </div>
-                        <div class="col-12">
-                <div class="card">
-                    <div class="card-body">
-                        <h5><a id="result"></a></h5>
-                        <p>結果をシェア<br>
-                            <a href="https://twitter.com" target="_blank"><i class="fab fa-twitter fa-2x"
-                                    style="color: #55acee;"></i>つぶやく</a>
-                        </p>
-                        <textarea class="form-control"
-                            area-label="With textarea">
-                        私の最強パートナーは<?php print($player_result['team'] . ' ' . $player_result['name'] . '選手！' . '#あなたと相性最強なNPB選手占い'); ?>
-                        </textarea>
+                        <div class="share">
+                            <i class="far fa-hand-point-down"></i>結果をシェア！<br />
+                            <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-text="私と相性が最強なのは <?php print($player_result['team'] . ' '
+                                                                                                                                            . $player_result['name'] . '選手！'); ?> " data-url="http://uranai_npb.com" data-hashtags="あなたと相性が最強な野球選手診断,どうぶつ占い" data-size="large" data-show-count="false">Tweet</a>
+                            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                        </div>
                     </div>
-                </div>
-            </div>
-                </div>
             </section>
     </div>
 
