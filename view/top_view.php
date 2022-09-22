a116c061051603e68d360fb2a5750e5adeff3f89
emuroku <m610310@gmail.com>
2022-09-22 23:50:44 +0900

    hタグとfooterタグの位置を修正

--- view/top_view.php
+++ view/top_view.php
@@ -7,14 +7,14 @@
     <title>あなたと相性が最強な野球選手診断</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    
+
     <!-- CSS -->
     <link rel="stylesheet" href="<?php print(STYLESHEET_PATH . '/top.css'); ?>">
     <link rel="stylesheet" href="<?php print(STYLESHEET_PATH . '/common.css'); ?>">
 
     <!-- icon -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-    
+
     <!-- OGP設定 -->
     <meta property="og:url" content="http://npburanai.com/top.php" />
     <meta property="og:type" content="website" />
@@ -29,49 +29,49 @@
     <!-- navigation -->
     <nav class="navbar navbar-expand-sm justify-content-center navbar-light bg-light">
         <div class="title">
-            <h2><i class="fas fa-baseball-ball"></i>あなたと相性が最強な野球選手診断<i class="fas fa-baseball-ball"></i></h2>
+            <h1><i class="fas fa-baseball-ball"></i>あなたと相性が最強な野球選手診断<i class="fas fa-baseball-ball"></i></h1>
         </div>
     </nav>
 
     <!-- container -->
     <div class="container">
         <div class="col-12">
-            <h5><i class="fa-solid fa-pen-field"></i>どうぶつ占いで、あなたと相性が最強の現役プロ野球選手を占います</h3>
-                <!-- 入力フォーム -->
-                <div class="input_birthday">
-                    <form method="post" action="result.php">
-                        <!-- CSRF対策 -->
-                        <div class="form-group col-12">
-                            <p>生年月日を入力してください</p>
-                            <div class="birthday">
-                                <input type="number" class="form-control form-control-lg col-6" name="year" required min="1926" max="2030" id="year" placeholder value="1990">
-                                <p class="birth_txt"><label for="year">年</label></p>
+            <h2><i class="fa-solid fa-pen-field"></i>どうぶつ占いで、あなたと相性が最強の現役プロ野球選手を占います</h2>
+            <!-- 入力フォーム -->
+            <div class="input_birthday">
+                <form method="post" action="result.php">
+                    <!-- CSRF対策 -->
+                    <div class="form-group col-12">
+                        <p>生年月日を入力してください</p>
+                        <div class="birthday">
+                            <input type="number" class="form-control form-control-lg col-6" name="year" required min="1926" max="2030" id="year" placeholder value="1990">
+                            <p class="birth_txt"><label for="year">年</label></p>
 
-                                <input type="number" class="form-control form-control-lg col-3" name="month" required min="1" max="12" id="month" placeholder value="1">
-                                <p class="birth_txt"><label for="month">月</label></p>
+                            <input type="number" class="form-control form-control-lg col-3" name="month" required min="1" max="12" id="month" placeholder value="1">
+                            <p class="birth_txt"><label for="month">月</label></p>
 
-                                <input type="number" class="form-control form-control-lg col-3" name="day" required min="1" max="31" id="day" placeholder value="1">
-                                <p class="birth_txt"><label for="day">日</label></p>
-                            </div>
-                            <div class="error_msg">
-                                <?php
-                                if ($error == 'true') {
-                                    print("存在しない日付は入力できません！");
-                                }; ?>
-                            </div>
-                            <div class="submit"><button class="btn btn-primary" id="btn_submit">占う！</button></div>
+                            <input type="number" class="form-control form-control-lg col-3" name="day" required min="1" max="31" id="day" placeholder value="1">
+                            <p class="birth_txt"><label for="day">日</label></p>
                         </div>
-                    </form>
-                </div>
+                        <div class="error_msg">
+                            <?php
+                            if ($error == 'true') {
+                                print("存在しない日付は入力できません！");
+                            }; ?>
+                        </div>
+                        <div class="submit"><button class="btn btn-primary" id="btn_submit">占う！</button></div>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
+    <footer>
+        <div class="copyright">
+            <p>© あなたと相性が最強な野球選手診断</p>
+            <p>By <a href="https://twitter.com/m610310">@m610310</a>
+            <p>
+        </div>
+    </footer>
 </body>
-<footer>
-    <div class="copyright">
-        <p>© あなたと相性が最強な野球選手診断</p>
-        <p>By <a href="https://twitter.com/m610310">@m610310</a>
-        <p>
-    </div>
-</footer>
 
 </html>
